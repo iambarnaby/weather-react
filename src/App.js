2adc6022780f0721864cd7a8de39e2f48de9ee38
lampshed <aperta.no@gmail.com>
2022-01-31 13:47:38 +0000

    displaying for info and some styling

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import { useEffect, useState } from "react";
 import Searchbar from "./Comps/Searchbar";
 
 function WeatherApp() {
-  console.log("changed");
   const [temp, setTemp] = useState("");
   const [weather, setWeather] = useState("");
   const [location, setLocation] = useState("London");
   const [iconURL, setIcon] = useState("");
+  const [feelsLike, setFeelsLike] = useState("");
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+  const [{ sunrise, sunset }, setRise] = useState({ sunrise: "", sunset: "" });
+  const [humidity, setHumidity] = useState("");
+  const [uvIndex, setUV] = useState("");
+  const [windspeed, setWindspeed] = useState("");
 
   useEffect(() => {
     getWeather();
@@ -20,7 +26,6 @@ function WeatherApp() {
 
   const setCityName = (cityName) => {
     setLocation(cityName);
-    console.log("test");
     getWeather();
   };
 
@@ -36,14 +41,36 @@ function WeatherApp() {
         setIcon(
           `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         );
+        setFeelsLike(data.main.feels_like);
+        setMin(data.main.temp_min);
+        setMax(data.main.temp_max);
+        setHumidity(data.main.humidity);
+        setWindspeed(data.wind.speed);
+        setRise({ sunrise: data.sys.sunrise, sunset: data.sys.sunset });
+        console.log(data);
       });
   }
 
   return (
     <div className="App">
       <Searchbar setCityName={setCityName} />
-      <WeatherInfo temp={{ loc: location, temp, weather: weather, iconURL }} />
+      <WeatherInfo
+        temp={{
+          loc: location,
+          temp,
+          weather: weather,
+          iconURL,
+          feelsLike,
+          max,
+          min,
+          windspeed,
+          humidity,
+          sunrise,
+          sunset,
+        }}
+      />
       <LiveFeed location={location} />
+
       <LocalNews />
     </div>
   );
--- src/Comps/WeatherInfo.js
+++ src/Comps/WeatherInfo.js
@@ -1,18 +1,72 @@
+import humidIcon from "./icons/humidity.png";
+import sunriseIcon from "./icons/sunrise.png";
+import sunsetIcon from "./icons/sunset.png";
+import windspeedIcon from "./icons/windspeed.png";
+import uvIndexIcon from "./icons/uvIndex.png";
+import Moment from "react-moment";
 const WeatherInfo = (props) => {
   return (
     <div className="weather-info-container">
       <div className="weather-info-grid">
         <div className="weather-info-head">
-          <div className="weather-info location">{props.temp.loc}</div>
-          <div className="weather-info time"></div>
+          <div className="weather-info-location">{props.temp.loc}</div>
 
-          <div className="weather-info weather">{props.temp.weather}</div>
+          <div className="weather-info-weather">{props.temp.weather}</div>
           <div className="icon-container">
             <img src={props.temp.iconURL} alt="icon"></img>
+            <div className="weather-info-temperature">{props.temp.temp}°</div>
           </div>
-          <div className="weather-info temperature">{props.temp.temp} °C</div>
+          <span>
+            <img
+              className="weather-info-icons"
+              src={sunriseIcon}
+              alt="sunrise"
+            ></img>
+            <Moment unix>{props.temp.sunrise}</Moment>
+          </span>
+          <span>
+            <img
+              className="weather-info-icons"
+              src={sunsetIcon}
+              alt="sunset"
+            ></img>
+            <Moment unix>{props.temp.sunset}</Moment>
+          </span>
         </div>
-        <div className="other-info">Other info</div>
+
+        <div className="other-info">
+          <div className="other-temp-info">
+            Feels like {props.temp.feelsLike}°
+            <div className="temp-max-min">
+              Max/Min: {props.temp.max}°/{props.temp.min}°
+            </div>
+          </div>
+          <div className="weather-info humidity">
+            <img
+              className="weather-info-icons"
+              src={humidIcon}
+              alt="Humidity"
+            ></img>
+            <span className="other-info-data"> {props.temp.humidity}%</span>
+          </div>
+          <div className="weather-info windspeed">
+            <img
+              className="weather-info-icons"
+              src={windspeedIcon}
+              alt="windspeed"
+            ></img>
+            <span className="other-info-data">{props.temp.windspeed} m/s</span>
+          </div>
+          <div className="weather-info uv-index">
+            <img
+              className="weather-info-icons"
+              src={uvIndexIcon}
+              alt="UV:"
+            ></img>{" "}
+            <span className="other-info-data">{props.temp.uvIndex}</span>
+          </div>
+          <div>8 day forcast below here</div>
+        </div>
       </div>
     </div>
   );
