817e4725cecc43f69c3fb4a5953c6f394ea2d26a
lampshed <aperta.no@gmail.com>
2022-02-07 14:59:15 +0000

    newsdata.io replacing newsapi.org some image requests are broken and after speaking with newsdata.io Hong Kong news should be fixed soon

--- src/Comps/LocalNews.js
+++ src/Comps/LocalNews.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import newsExample from "./newsExample.png";
 const LocalNews = (prop) => {
   const [{ articles, loading }, setArticles] = useState({
     articles: [],
@@ -10,35 +9,32 @@ const LocalNews = (prop) => {
     setArticles({ articles: [], loading: true });
     const response = axios
       .get(
-        `https://newsapi.org/v2/top-headlines?country=${prop.country}&pageSize=5&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+        `https://newsdata.io/api/1/news?apikey=${process.env.REACT_APP_NEWS_API_KEY}&language=en&country=${prop.country}`
       )
       .then((response) => {
         const data = response.data;
-        //console.log(data);
-        setArticles({ articles: data.articles, loading: false });
+        let half_length = Math.ceil(data.results.length / 2);
+        setArticles({
+          articles: data.results.splice(0, half_length),
+          loading: false,
+        });
       });
   }, [prop.country]);
+
+  console.log(articles);
 
   return (
     <div className="local-news-container">
       <h1>Local News</h1>
       <div className="articles">
-        {loading ? (
-          <div>
-            loading... newsapi.org now only allows free API calls locally I'll
-            be replacing the api soon. this is how it looks locally:
-            <img src={newsExample} alt="news"></img>
-          </div>
-        ) : (
-          <></>
-        )}
+        {loading ? <div>loading...</div> : <></>}
 
         {articles.map((article) => (
           <div key={article.title.toString()} className="article-container">
-            <a target="_blank" rel="noreferrer" href={article.url}>
+            <a target="_blank" rel="noreferrer" href={article.link}>
               <img
                 className="article-image"
-                src={article.urlToImage}
+                src={article.image_url}
                 alt="artlce"
               ></img>
               <div className="article-title">{article.title}</div>
